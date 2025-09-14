5c3c28f4f7c2313d8a54c589c90045bc8789528d
Enoch Baah <ebaah2942@gmail.com>
2025-09-14 12:56:28 +0000

    Changed the routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Router, Route, Link } from 'react-router-dom';
+import { Router, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
@@ -18,17 +18,12 @@ export default function App() {
       </header>
 
       <Router>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/edit/:id" element={<EditRecipeForm />} />
+        </Routes>
       </Router>
     </div>
   );
