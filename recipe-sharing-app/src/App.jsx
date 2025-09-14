f716ca519db2f7a4e8427637ddf5f66fb14f218a
Enoch Baah <ebaah2942@gmail.com>
2025-09-14 12:54:06 +0000

    New checks

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,34 +1,11 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Router, Route, Link } from 'react-router-dom';
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import RecipeDetails from './components/RecipeDetails'
 import EditRecipeForm from './components/EditRecipeForm'
-import DeleteRecipeButton from './components/DeleteRecipeButton'
 import './App.css'
-// import { useRecipeStore } from './recipeStore'
 
-// import useRecipeStore from './recipeStore'
-
-
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
-//         <h1>Recipe Sharing App</h1>
-//         <AddRecipeForm />
-//         <RecipeList />
-//       </div>
-      
-//     </>
-//   )
-// }
-
-// export default App
 export default function App() {
   return (
     <div style={{ maxWidth: 800, margin: "0 auto", padding: 20 }}>
@@ -40,7 +17,7 @@ export default function App() {
         </h1>
       </header>
 
-      <Routes>
+      <Router>
         <Route
           path="/"
           element={
@@ -52,7 +29,7 @@ export default function App() {
         />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
         <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
-      </Routes>
+      </Router>
     </div>
   );
 }
